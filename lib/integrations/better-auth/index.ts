2834ca8eb4bb5f99ab570caf5394d73400acfac5
Kabir Asani <iamkabirasani@gmail.com>
2025-05-05 00:29:44 +0530

    Remove baseURL from betterAuthClient configuration

--- lib/integrations/better-auth/index.ts
+++ lib/integrations/better-auth/index.ts
@@ -3,7 +3,6 @@ import { nextCookies } from "better-auth/next-js";
 import { webClientUrl } from "@/lib/extras/environment";
 
 export const betterAuthClient = createAuthClient({
-  baseURL: webClientUrl,
   basePath: "/authentications",
   plugins: [nextCookies()],
 });
