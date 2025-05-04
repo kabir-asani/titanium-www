906f6f2aa7eb442271f403396de2e99f8bbaf000
Kabir Asani <iamkabirasani@gmail.com>
2025-05-04 18:20:42 +0530

    Add SessionGuard component to protect routes based on user session

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import { SessionCard } from "@/components/session-card";
+import { SessionGuard } from "@/components/session-guard";
 
 const RootPage = () => {
   return (
-    <div className="h-svh flex items-center justify-center">
-      <SessionCard />
-    </div>
+    <SessionGuard>
+      <div className="h-svh flex items-center justify-center">
+        <SessionCard />
+      </div>
+    </SessionGuard>
   );
 };
 
